6de7296c188f4c421d7d110fff1cb26f539bd063
an4 <ad12461@my.bristol.ac.uk>
2016-02-16 15:22:21 +0000

    .

--- public/controller.js
+++ public/controller.js
@@ -8,12 +8,19 @@ angular.module('TheApp').controller("MainCtrl", ['$scope', '$http',
         /* [1] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Typed_arrays */
         var size = 8 * 1024 * 1024;
         var primeBuffer = new ArrayBuffer(size);
-        var primeView = new DataView(buffer);
+        var primeView = new DataView(primeBuffer);
 
         var offset = 64;
 
         /* We assume a victim system with 8192 cache sets, each which l=12-way associativity*/
-        var s = 8192;
+        var sets = 8192;
+
+        /* Global variable s so we can re-add it later if it is part of the same cache set */
+        var s;
+
+        /* Variable to check lines in the same cache set, maybe change value? Not sure
+        why it is set to 0.*/
+        var x = 0;
 
         /* 12-way associative */
         var l = 12;
@@ -29,11 +36,13 @@ angular.module('TheApp').controller("MainCtrl", ['$scope', '$http',
             S[i] = false;
         }
 
+        /* set threshold values between times */
+        var threshold = 1;
+
         /* Access the buffer in offsets of 1 per every 64 bytes. */
-        // TODO only access memebers that are in the set? if statement if S true/false
         function accessMembers(set) {
             for(var key in set) {
-                probeView.getUint32(key * offset);
+                primeView.getUint32(key * offset);
             }
         }
 
@@ -53,11 +62,13 @@ angular.module('TheApp').controller("MainCtrl", ['$scope', '$http',
                 s = -1;
             } else {
                 s = Math.floor(Math.random() * Object.keys(available).length);
-            //   delete set[available[s]]
-
+                delete set[available[s]]
             }
         }
 
+        function removeFromSameSet(el) {
+
+        }
 
         /**
          * Algorithm 1 Profiling a Cache Set
@@ -75,34 +86,61 @@ angular.module('TheApp').controller("MainCtrl", ['$scope', '$http',
          *              is not part of the same set as x.
          *      2. If |S| = 12, return S. Otherwise report failure.
          */
-        function algorithm1 (x) {
-            // a
-            accessMembers(S);
+        function algorithm1() {
+            var k = 100;
+            while(k-- > 0) {
+                // a
+                accessMembers(S);
 
-            // b
+                // b
 
-            // var current;
+                // var current;
+                // var startTime = window.performance.now();
+                // current = primeView.getUint32(x);
+                // var endTime = window.performance.now();
+                // var diffTimeBefore = endTime - startTime;
+
+                var diffTimeBefore = getTime(x);
+
+                // c
+                // Select random page s(global so it can be added later) from S and remove it.
+                removeRandomPage(S);
+
+                // d
+                accessMembers(S);
+
+                // e
+                var diffTimeAfter = getTime(x);
+                var difference = diffTimeBefore - diffTimeAfter;
+                console.log(difference);
+
+                // f
+                if(difference > threshold) {
+                    // Place s back into the set
+                    S[s] = true;
+                }
+
+                if(Object.keys(S).length == 12) {
+                    break;
+                }
+            }
+
+
+            // Example code from the paper.
+            // /* Invalidate the cache set */
+            // var currentEntry = startAddress;
+            // do {
+            //     currentEntry = probeView.getUint32(currentEntry);
+            // } while(currentEntry != startAddress);
+            //
+            // /* Measure access time */
             // var startTime = window.performance.now();
-            // current = primeView.getUint32(x);
+            // currentEntry = primeView.getUint32(variableToAccess);
             // var endTime = window.performance.now();
-            // var diffTimeBefore = endTime - startTime;
-
-            var diffTimeBefore = getTime(x);
-
-            // c
-            // Select random page s from S and remove it.
-            removeRandomPage(S);
-
-            /* Invalidate the cache set */
-            var currentEntry = startAddress;
-            do {
-                currentEntry = probeView.getUint32(currentEntry);
-            } while(currentEntry != startAddress);
-
-            /* Measure access time */
-            var startTime = window.performance.now();
-            currentEntry = primeView.getUint32(variableToAccess);
-            var endTime = window.performance.now();
-
         };
+        // end Algorithm1
+
+        console.log("Start algo 1");
+        algorithm1();
+        console.log("End algo 1");
 }]);
