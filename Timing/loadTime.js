17279ea0e997b6ceada81a882617853d1daf816e
an4 <ad12461@my.bristol.ac.uk>
2016-02-24 21:00:52 +0000

    Video timing working

---
+++ Timing/loadTime.js
@@ -0,0 +1,49 @@
+function time_image(url) {
+    var img = new Image();
+    img.onerror = function() {
+        var end = window.performance.now();
+        console.log("Time image: " + (end - start));
+    }
+    var start = window.performance.now();
+    img.src = url;
+};
+
+function time_video(url, name) {
+    var s = document.createElement('video');
+
+    s.onerror = function() {
+        timeError = window.performance.now();
+        console.log("Time video error: " + (timeError - timeLoad) + " " + name);
+    }
+
+    s.onloadstart = function() {
+        timeLoad = window.performance.now();
+    };
+
+    var start = window.performance.now(), timeLoad, timeCanPlay, timeError;
+    s.src = url;
+}
+
+function time_script(url) {
+    window.onerror = function() {
+        var d = performance.now() - window.start;
+        console.log("parsing done " + d);
+    };
+    var s = document.createElement('script');
+    s.type = "text/javascript";
+    document.body.appendChild(s);
+    s.onload = function() {
+        console.log("script downloaded");
+        window.start = window.performance.now();
+    };
+    s.src = url;
+};
+
+// time_video('https://www.facebook.com/kristianTonef');
+// time_video('https://www.facebook.com/alina.ivan.946');
+// time_video('https://www.facebook.com/logan.lerman.37');
+// time_video('https://www.facebook.com/jzelikovic');
+
+
+time_video('https://www.facebook.com/groups/208547725916026/', 'In');
+time_video('https://www.facebook.com/groups/852392078107320/', 'Out');
