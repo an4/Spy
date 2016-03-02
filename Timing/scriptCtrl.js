795cdd38f46d1feeeba06f77fcf5011d0f9037fa
an4 <ad12461@my.bristol.ac.uk>
2016-03-02 18:32:45 +0000

    Script still not working

--- Timing/scriptCtrl.js
+++ Timing/scriptCtrl.js
@@ -2,19 +2,52 @@
 
 var scriptCtrl = angular.module('ScriptCtrl', []);
 
-function time_script(url) {
+// function time_script(url) {
+//     window.onerror = function() {
+//         var d = performance.now() - window.start;
+//         console.log("parsing done " + d);
+//     };
+//     var s = document.createElement('script');
+//     document.body.appendChild(s);
+//     s.onload = function() {
+//         console.log("script downloaded");
+//         window.start = window.performance.now();
+//     };
+//     s.src = url;
+// };
+
+// $( document ).ready(function() {
+    console.log( "ready!" );
+
+    var s = document.createElement('script');
+
     window.onerror = function() {
         var d = performance.now() - window.start;
-        console.log("parsing done " + d);
+        console.log('parsing done', d);
+        // alert('parsing done: ' +  d);
     };
-    var s = document.createElement('script');
+
     document.body.appendChild(s);
+
     s.onload = function() {
-        console.log("script downloaded");
-        window.start = window.performance.now();
+        console.log('script downloaded');
+        window.start = performance.now();
+    }
+    // s.src = 'http://example.com/resource';
+
+    s.src = 'Files/test_50.js';
+
+    function time_script() {
+
+        s.src = 'Files/test_50.js';
     };
-    s.src = url;
-};
+
+// });
+
+
+
+
+
 
 // time_script('/Files/test_50.js');
 
