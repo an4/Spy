29c51af1e7abb7230815ad8dc078a73160b8112c
an4 <ad12461@my.bristol.ac.uk>
2016-04-06 12:14:54 +0100

    Ranges displayed

--- Size/controller.js
+++ Size/controller.js
@@ -53,22 +53,6 @@ angular.module('TheApp').controller('controller', ['$scope', '$http', '$location
 /////////////////////// Mutliple Files ////////////////////////
 ///////////////////////////////////////////////////////////////
         /**
-         * Build the results for each file.
-         */
-        function buildResult(times, file) {
-            var result = {};
-            var timingData = removeOutliers(times);
-            result.url = file.url;
-            result.times = times;
-            result.name = file.name;
-            result.size = file.size;
-            result.index = file.index;
-            result.mean = math.mean(timingData);
-            result.std = math.std(timingData);
-            return result;
-        }
-
-        /**
          * Get multiple measurements for the same file
          * file - file to be measured
          * rounds - how many measurements
@@ -197,6 +181,37 @@ angular.module('TheApp').controller('controller', ['$scope', '$http', '$location
             return data;
         };
 
+        /**
+         * Build the results for each file.
+         */
+        function buildResult(times, file) {
+            var result = {};
+            var timingData = removeOutliers(times);
+            result.url = file.url;
+            result.times = times;
+            result.name = file.name;
+            result.size = file.size;
+            result.index = file.index;
+            result.mean = math.mean(timingData);
+            result.std = math.std(timingData);
+            return result;
+        };
+
+        /**
+         * Returns the mean and original file size for each file object in the data
+         * array.
+         */
+        function getMeans(data) {
+            var output = [];
+            data.forEach(function(file) {
+                var el = {};
+                el.mean = file.mean;
+                el.name = file.name;
+                output.push(el);
+            });
+            return output;
+        };
+
 /////////////////////////////////////////////////////////
 ////////////////////////// GUESS ////////////////////////
 /////////////////////////////////////////////////////////
@@ -241,66 +256,18 @@ angular.module('TheApp').controller('controller', ['$scope', '$http', '$location
                 }
                 resolve(closest);
             });
-        }
+        };
 
-        $scope.guess = function() {
-            var base_url = "https://raw.githubusercontent.com/an4/Data-Storage/master/";
-
-            var files = [
-                {size: "50", url: base_url + "50kB.html", name: "50kB"},
-                {size: "100", url: base_url + "100kB.html", name: "100kB"},
-                {size: "150", url: base_url + "150kB.html", name: "150kB"},
-                {size: "200", url: base_url + "200kB.html", name: "200kB"},
-                {size: "250", url: base_url + "250kB.html", name: "250kB"},
-                {size: "300", url: base_url + "300kB.html", name: "300kB"},
-                {size: "350", url: base_url + "350kB.html", name: "350kB"},
-                {size: "400", url: base_url + "400kB.html", name: "400kB"},
-                {size: "450", url: base_url + "450kB.html", name: "450kB"},
-                {size: "500", url: base_url + "500kB.html", name: "500kB"},
-                {size: "550", url: base_url + "550kB.html", name: "550kB"},
-                {size: "600", url: base_url + "600kB.html", name: "600kB"},
-                {size: "650", url: base_url + "650kB.html", name: "650kB"},
-                {size: "700", url: base_url + "700kB.html", name: "700kB"},
-                // {size: "750", url: base_url + "750kB.html", name: "750kB"},
-                // {size: "800", url: base_url + "800kB.html", name: "800kB"},
-                // {size: "850", url: base_url + "850kB.html", name: "850kB"},
-                // {size: "900", url: base_url + "900kB.html", name: "900kB"},
-                // {size: "950", url: base_url + "950kB.html", name: "950kB"},
-                // {size: "1000", url: base_url + "1000kB.html", name: "1000kB"},
-                // {size: "1100", url: base_url + "1100kB.html", name: "1100kB"},
-                // {size: "1200", url: base_url + "1200kB.html", name: "1200kB"},
-                // {size: "1300", url: base_url + "1300kB.html", name: "1300kB"},
-                // {size: "1400", url: base_url + "1400kB.html", name: "1400kB"},
-                // {size: "1500", url: base_url + "1500kB.html", name: "1500kB"},
-                // {size: "1600", url: base_url + "1600kB.html", name: "1600kB"},
-                // {size: "1700", url: base_url + "1700kB.html", name: "1700kB"},
-                // {size: "1800", url: base_url + "1800kB.html", name: "1800kB"},
-                // {size: "1900", url: base_url + "1900kB.html", name: "1900kB"},
-                // {size: "2000", url: base_url + "2000kB.html", name: "2000kB"}
-            ];
-
-            // Add original index for each file
-            for(var i=0; i<files.length; i++) {
-                files[i].index = i;
-            }
-            files = shuffle(files);
-
-            // var guess_file_url = "https://raw.githubusercontent.com/an4/Data-Storage/master/256kB.html";
-            // var guess_file_url = "https://raw.githubusercontent.com/an4/Data-Storage/master/512kB.html";
-            // var guess_file_url = "https://raw.githubusercontent.com/an4/Data-Storage/master/128kB.html";
-
-            var guess_file_url = $scope.guess.path;
-            var guess = {size: "unknown", url: guess_file_url, name: "unknown"};
-
-            // var rounds = $scope.guess.rounds;
-            var rounds = 50;
-
-            getMeasurementAll(files, rounds, measureTimeVideo).then(function(results) {
-                getMeasurementFile(guess, rounds, measureTimeVideo).then(function(guessResult) {
-                    getRange(results, guessResult).then(function(result) {
-                    // closestMean(results, guessResult).then(function(result) {
-                        console.log(result);
-                    })
+        /**
+         * Returns a promise that resolves to an array of means for each file.
+         */
+        function computeMeans() {
+            return new Promise(function(resolve, reject) {
+                var files = getFiles(true);
+                var rounds = 100;
+                getMeasurementAll(files, rounds, measureTimeVideo).then(function(results) {
+                    var means = getMeans(results);
+                    resolve(means);
                 });
             });
         };
@@ -387,76 +354,33 @@ angular.module('TheApp').controller('controller', ['$scope', '$http', '$location
         };
 
 /////////////////////////////////////////////////////////
-////////////////// BUTTONS & UI /////////////////////////
+////////////////////// DATA /////////////////////////////
 /////////////////////////////////////////////////////////
 
-        $scope.run = function() {
-            var path = "/Files/";
-
-            $scope.settings.progress = 0;
-
-            console.log("Running...");
-
-            // Change this based on URL
-            var type = "";
-            if($location.path() === '/cache') {
-                type = "cache";
-            }
-            if($location.path() === '/basic') {
-                type = "test";
-            }
-
-            var tag = $scope.settings.type;
-            var method = measureTimeVideo;
-            if(tag === 'video') {
-                method = measureTimeVideo;
-            }
-            if(tag === 'image') {
-                method = measureTimeImage;
-            }
-
+        /**
+         * Returns an array of file objects, containing the size, path, index, and
+         * name of the file. By default the file paths are from the GitHub repository
+         * (remote is true). If remore is false the files paths link to a server
+         * running locally. The files are shuffeled before being returned.
+         */
+        function getFiles(remote) {
             var base_url = "https://raw.githubusercontent.com/an4/Data-Storage/master/";
 
-            var cache_base_url = "https://raw.githubusercontent.com/an4/Remote-File-Server---App-Cache/master/";
+            if(!remote) {
+                var base_url = "http://localhost:3000/";
+            }
 
-            // base_url = cache_base_url;
-
-            // var base_url = "http://localhost:3000/";
-
-            // var files = [
-            //     // {size:   "50", url: path+type+"_50.html",   name: "50kB"},
-            //     {size:  "100", url: path+type+"_100.html",  name: "100kB"},
-            //     // {size:  "150", url: path+type+"_150.html",  name: "150kB"},
-            //     {size:  "200", url: path+type+"_200.html",  name: "200kB"},
-            //     // {size:  "250", url: path+type+"_250.html",  name: "250kB"},
-            //     {size:  "300", url: path+type+"_300a.html", name: "300kB"},
-            //     // {size:  "350", url: path+type+"_350.html",  name: "350kB"},
-            //     {size:  "400", url: path+type+"_400.html",  name: "400kB"},
-            //     // {size:  "450", url: path+type+"_450.html",  name: "450kB"},
-            //     {size:  "500", url: path+type+"_500.html",  name: "500kB"},
-            //     // {size:  "550", url: path+type+"_550.html",  name: "550kB"},
-            //     {size:  "600", url: path+type+"_600.html",  name: "600kB"},
-            //     // {size:  "650", url: path+type+"_650.html",  name: "650kB"},
-            //     {size:  "700", url: path+type+"_700.html",  name: "700kB"},
-            //     // {size:  "750", url: path+type+"_750.html",  name: "750kB"},
-            //     {size:  "800", url: path+type+"_800.html",  name: "800kB"},
-            //     // {size:  "850", url: path+type+"_850.html",  name: "850kB"},
-            //     {size:  "900", url: path+type+"_900.html",  name: "900kB"},
-            //     // {size:  "950", url: path+type+"_950.html",  name: "950kB"},
-            //     {size: "1000", url: path+type+"_1000.html", name: "1000kB"}
-            // ];
-
-            // var files = [
-            //     {size: "50", url: base_url + "50kB.html", name: "50kB"},
-            //     {size: "100", url: base_url + "100kB.html", name: "100kB"},
-            //     {size: "150", url: base_url + "150kB.html", name: "150kB"},
-            //     {size: "200", url: base_url + "200kB.html", name: "200kB"},
-            //     {size: "250", url: base_url + "250kB.html", name: "250kB"},
-            //     {size: "300", url: base_url + "300kB.html", name: "300kB"},
-            //     {size: "350", url: base_url + "350kB.html", name: "350kB"},
-            //     {size: "400", url: base_url + "400kB.html", name: "400kB"},
-            //     {size: "450", url: base_url + "450kB.html", name: "450kB"},
-            //     {size: "500", url: base_url + "500kB.html", name: "500kB"},
+            var files = [
+                {size: "50", url: base_url + "50kB.html", name: "50kB"},
+                {size: "100", url: base_url + "100kB.html", name: "100kB"},
+                {size: "150", url: base_url + "150kB.html", name: "150kB"},
+                {size: "200", url: base_url + "200kB.html", name: "200kB"},
+                {size: "250", url: base_url + "250kB.html", name: "250kB"},
+                {size: "300", url: base_url + "300kB.html", name: "300kB"},
+                {size: "350", url: base_url + "350kB.html", name: "350kB"},
+                {size: "400", url: base_url + "400kB.html", name: "400kB"},
+                // {size: "450", url: base_url + "450kB.html", name: "450kB"},
+                // {size: "500", url: base_url + "500kB.html", name: "500kB"},
                 // {size: "550", url: base_url + "550kB.html", name: "550kB"},
                 // {size: "600", url: base_url + "600kB.html", name: "600kB"},
                 // {size: "650", url: base_url + "650kB.html", name: "650kB"},
@@ -476,12 +400,9 @@ angular.module('TheApp').controller('controller', ['$scope', '$http', '$location
                 // {size: "1700", url: base_url + "1700kB.html", name: "1700kB"},
                 // {size: "1800", url: base_url + "1800kB.html", name: "1800kB"},
                 // {size: "1900", url: base_url + "1900kB.html", name: "1900kB"},
-                // {size: "2000", url: base_url + "2000kB.html", name: "2000kB"}
-            // ];
-
-            var external_files = [
-                {size: "16", url: base_url + "16kB.html", name: "16kB"},
-                {size: "32", url: base_url + "32kB.html", name: "32kB"},
+                // {size: "2000", url: base_url + "2000kB.html", name: "2000kB"},
+                // {size: "16", url: base_url + "16kB.html", name: "16kB"},
+                // {size: "32", url: base_url + "32kB.html", name: "32kB"},
                 // {size: "64", url: base_url + "64kB.html", name: "64kB"},
                 // {size: "128", url: base_url + "128kB.html", name: "128kB"},
                 // {size: "256", url: base_url + "256kB.html", name: "256kB"},
@@ -491,22 +412,85 @@ angular.module('TheApp').controller('controller', ['$scope', '$http', '$location
                 // {size: "4096", url: base_url + "4096kB.html", name: "4096kB"}
             ];
 
-            var files = external_files;
-
             // Add original index for each file
             for(var i=0; i<files.length; i++) {
                 files[i].index = i;
             }
 
-            $scope.settings.progressPart = 100/files.length;
+            return shuffle(files);
+        };
 
-            files = shuffle(files);
+/////////////////////////////////////////////////////////
+////////////////// BUTTONS & UI /////////////////////////
+/////////////////////////////////////////////////////////
+
+        $scope.run = function() {
+            var path = "/Files/";
+
+            $scope.settings.progress = 0;
+
+            var tag = $scope.settings.type;
+            var method = measureTimeVideo;
+            if(tag === 'video') {
+                method = measureTimeVideo;
+            }
+            if(tag === 'image') {
+                method = measureTimeImage;
+            }
 
             var rounds = $scope.settings.rounds;
+
+            var files = getFiles(true);
 
             getMeasurementAll(files, rounds, method).then(function(results) {
                 $scope.chartObject = {};
                 draw(results);
+            });
+        };
+
+        $scope.computeAndDisplayRanges = function() {
+            computeMeans().then(function(means) {
+                $scope.guess.ranges = [];
+                var range = {};
+                // 0 - first
+                range.size = '0kB - ' + means[0].name;
+                range.time = '0s - ' + means[0].mean + 's';
+                $scope.guess.ranges.push(range);
+
+                for(var i=1; i<means.length; i++) {
+                    range = {};
+                    range.size = means[i-1].name + ' - ' + means[i].name;
+                    range.time = means[i-1].mean + 's - ' + means[i].mean + 's';
+                    $scope.guess.ranges.push(range);
+                }
+
+                // > last
+                range = {};
+                range.size = '>' + means[means.length-1].name;
+                range.time = '>' + means[means.length-1].mean + 's';
+                $scope.guess.ranges.push(range);
+                $scope.$apply();
+            });
+        };
+
+        $scope.guess = function() {
+            var guess_file_url = $scope.guess.path;
+            var guess = {size: "unknown", url: guess_file_url, name: "unknown"};
+
+            var files = getFiles(true);
+
+            // var rounds = $scope.guess.rounds;
+            var rounds = 50;
+
+            console.log("Scope rounds: " + $scope.guess.rounds);
+
+            getMeasurementAll(files, rounds, measureTimeVideo).then(function(results) {
+                getMeasurementFile(guess, rounds, measureTimeVideo).then(function(guessResult) {
+                    getRange(results, guessResult).then(function(result) {
+                    // closestMean(results, guessResult).then(function(result) {
+                        console.log(result);
+                    })
+                });
             });
         };
 }]);
